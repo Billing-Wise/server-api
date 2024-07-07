835e311a0b4f822e9118b5960b16830ceb4b77fb
이우성 <dtd1614@gmail.com>
2024-07-08 05:18:03 +0900

    [test] checkstyle 검사 테스트

--- src/main/java/site/billingwise/api/serverapi/ServerApiApplication.java
+++ src/main/java/site/billingwise/api/serverapi/ServerApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApiApplication {
 
     public static void main(String[] args) {
-            SpringApplication.run(ServerApiApplication.class, args);
+        SpringApplication.run(ServerApiApplication.class, args);
     }
 
 }
