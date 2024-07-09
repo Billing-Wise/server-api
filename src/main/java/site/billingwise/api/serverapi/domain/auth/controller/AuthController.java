3444923ee5ab3544a79ec65f8f1a90e0f42cf090
이우성 <dtd1614@gmail.com>
2024-07-09 13:58:31 +0900

    [K5P-44] [feat] 로그인

--- src/main/java/site/billingwise/api/serverapi/domain/auth/controller/AuthController.java
+++ src/main/java/site/billingwise/api/serverapi/domain/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import site.billingwise.api.serverapi.domain.auth.dto.LoginDto;
 import site.billingwise.api.serverapi.domain.auth.dto.RegisterDto;
 import site.billingwise.api.serverapi.domain.auth.service.AuthService;
 import site.billingwise.api.serverapi.global.response.BaseResponse;
@@ -21,6 +22,13 @@ public class AuthController {
     public BaseResponse register(@Valid @RequestBody RegisterDto registerDto) {
         authService.register(registerDto);
         return new BaseResponse(SuccessInfo.REGISTER);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/login")
+    public BaseResponse login(@Valid @RequestBody LoginDto loginDto) {
+        authService.login(loginDto);
+        return new BaseResponse(SuccessInfo.LOGIN);
     }
 
 }
---
+++ src/main/java/site/billingwise/api/serverapi/domain/auth/service/CustomUserDetailService.java
@@ -0,0 +1,28 @@
+package site.billingwise.api.serverapi.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import site.billingwise.api.serverapi.domain.auth.CustomUserDetails;
+import site.billingwise.api.serverapi.domain.user.User;
+import site.billingwise.api.serverapi.domain.user.repository.UserRepository;
+import site.billingwise.api.serverapi.global.exception.GlobalException;
+import site.billingwise.api.serverapi.global.response.info.FailureInfo;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new GlobalException(FailureInfo.NOT_EXIST_USER));
+        return new CustomUserDetails(user);
+    }
+
+
+}
