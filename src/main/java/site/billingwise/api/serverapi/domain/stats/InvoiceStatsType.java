74fe512f395068d98bfeee06e7f8325ba001a54a
이우성 <dtd1614@gmail.com>
2024-07-31 13:56:37 +0900

    [refactor] enum 수정

--- src/main/java/site/billingwise/api/serverapi/domain/stats/InvoiceStatsType.java
+++ src/main/java/site/billingwise/api/serverapi/domain/stats/InvoiceStatsType.java
@@ -7,8 +7,8 @@ import site.billingwise.api.serverapi.domain.common.EnumField;
 @RequiredArgsConstructor
 public enum InvoiceStatsType implements EnumField {
 
-    WEEK(1L, "주간"),
-    MONTH(2L, "월간");
+    WEEKLY(1L, "주간"),
+    MONTHLY(2L, "월간");
 
     private final Long id;
     private final String name;
